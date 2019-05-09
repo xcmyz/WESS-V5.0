946ef79884c8296a159556896dbbb9f804cc807d
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-05-09 14:31:27 +0800

    Add files via upload

--- visualize_loss/visualize.py
+++ visualize_loss/visualize.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def cut_arr(arr):
+    for index in range(np.shape(arr)[0]):
+        if arr[index] >= 0.5:
+            arr[index] = 0.5
+
+    return arr
+
+
 def visualize(total_loss_file_name, mel_loss_file_name, gate_loss_file_name):
     plt.figure()
 
@@ -11,7 +19,7 @@ def visualize(total_loss_file_name, mel_loss_file_name, gate_loss_file_name):
             total_loss_arr = np.append(total_loss_arr, float(loss))
 
     x = np.array([i for i in range(np.shape(total_loss_arr)[0])])
-    y = total_loss_arr
+    y = cut_arr(total_loss_arr)
 
     plt.plot(x, y, color="y", lw=0.7, label="total loss")
 
@@ -21,7 +29,7 @@ def visualize(total_loss_file_name, mel_loss_file_name, gate_loss_file_name):
             mel_loss_arr = np.append(mel_loss_arr, float(loss))
 
     x = np.array([i for i in range(np.shape(mel_loss_arr)[0])])
-    y = mel_loss_arr
+    y = cut_arr(mel_loss_arr)
 
     plt.plot(x, y, color="r", lw=0.7, label="mel loss")
 
@@ -31,7 +39,7 @@ def visualize(total_loss_file_name, mel_loss_file_name, gate_loss_file_name):
             gate_loss_arr = np.append(gate_loss_arr, float(loss))
 
     x = np.array([i for i in range(np.shape(gate_loss_arr)[0])])
-    y = gate_loss_arr
+    y = cut_arr(gate_loss_arr)
 
     plt.plot(x, y, color="b", lw=0.7, label="gate loss")
 
